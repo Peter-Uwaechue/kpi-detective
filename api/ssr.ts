a2aed698233c54d32a7693ac81249a86c051a4e0
Peter Uwaechue <uwaechuepeter2@gmail.com>
2026-08-16 09:29:47 +0000

    Fix Vercel SSR function types

--- api/ssr.ts
+++ api/ssr.ts
@@ -1,9 +1,9 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
+import type { Request, Response } from "express";
 import { createApp } from "../server/_core/index";
 
 let appPromise: ReturnType<typeof createApp> | undefined;
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: Request, res: Response) {
   appPromise ??= createApp();
   const app = await appPromise;
   return app(req, res);
